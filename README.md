192ef5c3d88af1315fa0a6697d35ddc9aa1f1df8
prem <itsmegauravrajbanshi@gmail.com>
2026-03-07 22:17:48 +0400

    Revert "creating the branch and add the RPS game"
    This reverts commit 74dc94afdfa64bf1c9eee263aff2b8b8d9e52917.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Python_Project
 Games
+<<<<<<< HEAD
 
  
 -> git branch branch-name            #create a branch-name = "version-1"
@@ -7,3 +8,5 @@ Games
 -> git push -u origin branch-name    #push the branch in git 
 -> git checkout branch-name          #switch to the branch   
 -> echo "test" >> test.sh
+=======
+>>>>>>> parent of 74dc94a (creating the branch and add the RPS game)
--- RPS.py
+++ RPS.py
@@ -1,64 +1,73 @@
+from ast import Constant
 import random
+round = total_win = total_tie = total_loss = 0
 
-if __name__ == "__main__":
-    ROCK = "r"
-    PAPER = "p"
-    SCISSORS = "s"
+def get_choice():
+    your_choice = input("-> Enter your choice (R, P, S): ").lower()
+    computer_choice = random.choice(choices)
+    return your_choice, computer_choice
 
-    print("Welcome to the Rock, Paper, Scissors Game!")
-    choices = [ROCK, PAPER, SCISSORS]
-    life = 3
-    won = loss = tie = 0
-    total_win = total_loss = total_tie = 0
-    total_rounds = 1
-    print("\n")
-    while True and life > 0:
-        your_choice = input("Enter your choice (r, p, s): ").lower()
-        
-        computer_choice = random.choice(choices)
+def display_score(life, win, loss, tie)-> bool:
+    global round, total_win, total_loss, total_tie
+    if life == 3:
+        round += 1
+        if win >= 2:
+            score(win, loss, tie)
+            print("Congratulations! You won the round!")
+            total_win += 1
+        elif loss >= 2:
+            score(win, loss, tie)
+            print("Sorry! You lost the round!")
+            total_loss += 1
+        else:
+            score(win, loss, tie)
+            print("It's a tie round!")
+            total_tie += 1
+        # print(f"Total Wins: {total_win}, Losses: {total_loss}, Total tie: {total_tie}")
+        should_continue = input("Press 'any key' to Play Again or 'q' to quit/total score: ").lower()
+        if should_continue == 'q':
+            print(f"-------------Total {round} Round Score------------")
+            print(f"Total Wins: {total_win}, Losses: {total_loss}, Total tie: {total_tie}")
+            print ("------------------------------------")
+            print("Thanks for playing! Goodbye!")
+            return
+        return True
+    
+def score(win, loss, tie):
+    print(f"---------{round} Round Score--------")
+    print(f"Wins: {win}, Losses: {loss}, Ties: {tie}")
+    print(f"--------------------------------")
+
+def play_game()-> None:
+    life = win = loss = tie = 0
+    while True and life < 3:
+        your_choice, computer_choice = get_choice()
 
         if your_choice not in choices:
-            print("Invalid choice. Please enter r/p/s.")
+            print("Invalid choice. Please enter R/P/S.")
             continue
 
         elif your_choice == computer_choice:
             print("It's a tie!")
             tie += 1
-            
         elif (your_choice == ROCK and computer_choice == SCISSORS) or\
                 (your_choice == PAPER and computer_choice == ROCK) or\
                 (your_choice == SCISSORS and computer_choice == PAPER):
             print("You win!")
-            won += 1
+            win += 1
         else:
             print("You lose!")
             loss += 1
-        life -= 1
-        if life > 0:
-            continue
-        if won >= 2:
-            print(f"{total_rounds} round You won. Well done!")
-            total_win += 1
-        elif loss >= 2:
-            print(f"{total_rounds} round You lost. Better luck next time!")
-            total_loss += 1
-        else:
-            print(f"{total_rounds} round It's a tie. Try again!")
-            total_tie += 1
-        # total_loss += loss
-        # total_win += won
-        # total_tie += tie
-        yes_no = input("Press ANY KEY to continue or 'n' to quit: ").lower()
-        if yes_no == 'n':
-            print("Thanks for playing. Goodbye!")
-            print(f"Total wins: {total_win}")
-            print(f"Total losses: {total_loss}")
-            print(f"Total ties: {total_tie}")
-            print(f"Total rounds: {total_rounds}")
-            break
-        total_rounds += 1
-        counter = 0
-        life = 3
-        won = 0
-        loss = 0
-        tie = 0
+        life += 1
+        reset_counter = display_score(life, win, loss, tie)
+        if reset_counter:
+            life = win = loss = tie = 0
+            
+        
+if __name__ == "__main__":
+    ROCK : Constant = "r"
+    PAPER : Constant = "p"
+    SCISSORS : Constant = "s"
+    choices = [ROCK, PAPER, SCISSORS]
+    print("Welcome to the Rock, Paper, Scissors Game!")
+    play_game()

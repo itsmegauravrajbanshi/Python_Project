19a165348e7d4e1c8d02d39907574648ec4a6fcd
prem <itsmegauravrajbanshi@gmail.com>
2026-03-21 21:55:43 +0400

    refactor simple_text_ediotr app

--- simple_text_editor.py
+++ simple_text_editor.py
@@ -15,22 +15,42 @@ def join_text():
             break
         lines.append(line)
     return "\n".join(lines), signal
-        
-def text_function():
-    while True:
-        print("1. Overwrite the file \n2. Append new text")
-        try:
-            choice = int(input())
-            if choice == 1:
-                file_mode(WRITE)
-                break
-            elif choice == 2:
-                file_mode(APPEND)
+
+def search_and_replace():
+    with open(file_name, 'r')as file:
+        content = file.read()
+        print(content)
+        while True:
+            old_text = input("Search text: ")
+            if old_text in content:
+                replace_text = input("Replace text: ")
+                content = content.replace(old_text, replace_text)
+                with open(file_name, 'w')as file:  
+                    file.write(content)
+                    print("Replaced successfully")
                 break
             else:
-                print("Enter correct choice.")
+                print("Text not found. Try Again!!!")
+             
+def text_function():
+    while True:
+        print("1. Overwrite the file \n2. Append new text. \n3. Search and Replace \n4. Exit")
+        try:
+            choice = int(input("Enter choice (1-4):"))
+            if choice == 1:
+                file_mode(WRITE)
+                continue
+            elif choice == 2:
+                file_mode(APPEND)
+                continue
+            elif choice == 3:
+                search_and_replace()
+            elif choice == 4:
+                break
+            else:
+                print("Enter correct choice!!!")
         except ValueError as ve:
-            print("Enter number only.")
+            print("Enter number only!!!")
 
 def file_mode(mode):
     print("Enter your text (type 'SAVE' on a new line to save and exit or 'EXIT' to exit without save.):")
@@ -42,14 +62,17 @@ def file_mode(mode):
     else:
         print(f"File {file_name} doesn't saved." )
 
-file_name = input("Enter the filename to open or create: ")
+def is_file_exist():
+    if Path(file_name).is_file():
+        print(f"{file_name} found.")
+        text_function()
+    else:
+        print(f"{file_name} not found. Creating a new file..")        
+        open(file_name,APPEND).close()
+        file_mode(APPEND)
 
-if Path(file_name).is_file():
-    print(f"{file_name} found.")
-    text_function()
-else:
-    print(f"{file_name} not found. Creating a new file..")        
-    open(file_name,APPEND).close()
-    file_mode(APPEND)
+if __name__ == '__main__':
+    file_name = input("Enter the filename to open or create: ")
+    is_file_exist()
 
     

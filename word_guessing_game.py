2cedfa9387a8217a92f33a940a9e838d4828cbc3
prem <itsmegauravrajbanshi@gmail.com>
2026-04-09 01:56:13 +0400

    Adding hangman, level and score board features in game.

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,20 +1,91 @@
 import random
+import string
 
+hangman = {
+    "0" : ("   _____ ",
+            "   |   | ",
+            "       | ",
+            "       | ",
+            "       | ",
+            "       | ",
+            "=======#=="),
+    "1" : ("   ____  ",
+            "   |   | ",
+            "   O   | ",
+            "       | ",
+            "       | ",
+            "       | ",
+            "=======#=="),
+    "2" : ("   ____  ",
+            "   |   | ",
+            "   O   | ",
+            "  /    | ",
+            "       | ",
+            "       | ",
+            "=======#=="),
+    "3" : ("   ____  ",
+            "   |   | ",
+            "   O   | ",
+            "  /|   | ",
+            "       | ",
+            "       | ",
+            "=======#=="),
+    "4" : ("   ____  ",
+            "   |   | ",
+            "   O   | ",
+            "  /|\\  | ",
+            "       | ",
+            "       | ",
+            "=======#=="),
+    "5" : ("   ____  ",
+            "   |   | ",
+            "   O   | ",
+            "  /|\\  | ",
+            "  /    | ",
+            "       | ",
+            "=======#=="),
+    "6" : ("   ____  ",
+            "   |   | ",
+            "   @   | ",
+            "  /|\\  | ",
+            "  / \\  | ",
+            "       | ",
+            "=======#=="),
+}
+
+def choose_word(text, min_word_length, max_word_length):
+    while True:
+        line = random.randint(1,8906)
+        word = text[line].upper().rstrip() # rstrip() function remove the "\n"
+        if min_word_length <= len(word) < max_word_length:
+            return word
+        
 def choice_level():
     file_name = "english.txt"
     word = None
     with open(file_name, 'r') as file:
         text = file.readlines()
+    print("Choose Level below :- ")
+    print("Press 'E' for Easy.")
+    print("      'M' for Medium.")
+    print("      'H' for Hard.")
     while True:
-        word_length = int(input("Please enter word length: "))
-        if word_length >= 4:
+        level = input("level : ").lower()
+        if level =='e':
+            print(level)
+            word = choose_word(text, 4, 6)
             break
-        print("Invalid length!")
-    while True:
-        line = random.randint(1,8906)
-        word = text[line].upper().rstrip() # rstrip() function remove the "\n"
-        if len(word) == word_length: 
+        elif level == "m":
+            print(level)
+            word = choose_word(text, 6, 8)
             break
+        elif level == "h":
+            print(level)
+            word = choose_word(text, 8, 15)
+            break
+        else:
+            print("Invalid level !!!")
+            continue
     return word
 
 def game_hint(display_word, word):
@@ -23,19 +94,49 @@ def game_hint(display_word, word):
             display_word[i] = word[i]
             break
 
-def play_game():
+def display_hangman(life):
+    for i in hangman:
+            if i == str(life):
+                for i in hangman[i]:
+                    print(i)
+
+def display_win(word, display_word):
+    if ("_" not in display_word):
+        print("\nWord is "+word+"")
+        print("-"*25)
+        print("Conguratulation! You have won.")
+        print("-"*25)
+        return True
+    
+def game_over():
+    print("-"*25)
+    for i in hangman["6"]:
+        print(i)
+    print("Game Over!!!")
+    print("-"*25)
+
+def play_game(won):
     word = choice_level()  
     print(word)  
     display_word = ['_'] * len(word)
     word_set = set()
-    Good_guess = False
-    while True:
+    Good_guess = None
+    life = 0
+    point = 0
+    while life < 6:
+        display_hangman(life)
+        Good_guess = False
+        print(f"\nYou have {6-life} guess.")
         print(*display_word)
         letter = input("Guess a letter (For Hint: Press '1'): ").upper()
+        if letter == "1":
+            game_hint(display_word, word)
+        if letter not in string.ascii_uppercase:
+            print("Enter only single alphabet character!")
+            continue
         if letter in word_set:
             print("Already Guess")
-        elif letter == "1":
-            game_hint(display_word, word)
+            continue
         else:
             for i in range(len(word)):
                 if word[i] == letter:
@@ -45,14 +146,25 @@ def play_game():
             if Good_guess:
                 print("Good guess")
             else:
+                life += 1
                 print("Wrong guess")
-        if (display_word == [w for w in word]):
-            print("\nWord is "+word+"")
-            print("Conguratulation! You have won.")
+        if display_win(word, display_word):
+            point += 1
             break
+    if life == 6:
+        game_over()
+    won.append(point)
 
 if __name__ == "__main__":
-    play_game()
+    won = []
+    while True:
+        play_game(won)
+        play_agin = input("Play Again (y/n): ").lower()
+        if play_agin == "n":
+            break
+    print("-------Score Board-------")
+    for i, w in enumerate(won):
+        print(f"Round {i+1}: {"Win" if w == 1 else "Loose"}")
 
 
         
